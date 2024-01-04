fff4dfaebd46f06041472b345d6432514e91fa02
Christoph Schmidt <christoph.,schmidt@tugraz.at>
2024-01-04 15:21:16 +0100

    Fixed IDE related errors in version 1.0.0.

--- src/CaptDeviceControl/__init__.py
+++ src/CaptDeviceControl/__init__.py
@@ -4,3 +4,8 @@ Author(s): Christoph Schmidt <christoph.schmidt@tugraz.at>
 Created: 2023-10-19 12:35
 Package Version: 
 """
+import sys
+from .CaptDeviceConfig import CaptDeviceConfig as Config
+from .controller.BaseAD2CaptDevice import BaseAD2CaptDevice as Controller
+from .model.AD2CaptDeviceModel import AD2CaptDeviceModel as Model
+from .view.AD2CaptDeviceView import ControlWindow as View
