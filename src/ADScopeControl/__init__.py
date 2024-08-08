8aedcb15c8a4d40fe8582100c687d91951c8ee05
Christoph Schmidt <christoph.schmidt@tugraz.at>
2024-08-08 13:14:32 +0200

    Updated to version to 1.1.7. Added a Helper, to more efficiently find the pyproject.toml

--- src/ADScopeControl/__init__.py
+++ src/ADScopeControl/__init__.py
@@ -5,27 +5,41 @@ Created: 2023-10-19 12:35
 Package Version: 
 """
 
-
 import ctypes
+import os
 import pathlib
 from pathlib import Path
 
 from WidgetCollection.Tools.PyProjectExtractor import extract_pyproject_info
+
+from . import Helpers
+
 # Directly in the repo
 
-pytoml = pathlib.Path(__file__).parent.parent.parent
-if not (pytoml / "pyproject.toml").exists():
-    # if installed via pip
-    pytoml = pathlib.Path(__file__)
+# ======================================================================================================================
+# The pyconfig.toml file is needed, to get the metadata. Depending on the installation method (pip or git) the file
+# is found in different places.
+# ======================================================================================================================
+pytoml = Helpers.get_pyprojecttoml()
 
-__version__ = extract_pyproject_info(pytoml,"version")
-__author__ = extract_pyproject_info(pytoml,"author")
-__description__ = extract_pyproject_info(pytoml,"description")
-__license__ = extract_pyproject_info(pytoml,"license")
-__url__ = extract_pyproject_info(pytoml,"url")
 
+def try_and_set(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        print(f"Error reading '{args[1]}' from {pathlib.Path(args[0])}: {e}")
+        return "unknown"
+
+
+__rootdir__ = os.path.dirname(os.path.realpath(__file__))
+__version__ = try_and_set(extract_pyproject_info, pytoml.parent, "version")
+__author__ = try_and_set(extract_pyproject_info, pytoml.parent, "author")
+__description__ = try_and_set(extract_pyproject_info, pytoml.parent, "description")
+__license__ = try_and_set(extract_pyproject_info, pytoml.parent, "license")
+__url__ = try_and_set(extract_pyproject_info, pytoml.parent, "url")
 # For correctly display the icon in the taskbar
-myappid = f'agentsmith29.ADScopeControl.{__version__}'  # arbitrary string
+
+myappid = f'agentsmith29.ADScopeControl.{__version__}'
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 from .CaptDeviceConfig import CaptDeviceConfig as Config
